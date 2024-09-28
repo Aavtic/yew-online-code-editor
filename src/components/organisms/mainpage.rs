b46bb6be04324b83b37b971c6cc451db51e04e3e
Aavtic <aadishm79@gmail.com>
2024-09-29 01:20:44 +0530

    Updated Code execution.

--- src/components/organisms/mainpage.rs
+++ src/components/organisms/mainpage.rs
@@ -6,11 +6,11 @@ use crate::components::atoms::{
     main_heading::MainHeading, 
     text_area::TextArea, 
     run_button:: RunButton, 
-    language_selector::LanguageSelector
+    language_selector::LanguageSelector,
 };
 use gloo::console::log;
 use serde::{Serialize, Deserialize};
-use serde_json::{json, to_string_pretty};
+use serde_json::to_string_pretty;
 use reqwasm::http::Request;
 
 
@@ -18,6 +18,11 @@ use reqwasm::http::Request;
 struct Data {
     pub code: String,
     pub language: String,
+}
+
+#[derive(Default)]
+struct OutputState {
+    output: String
 }
 
 #[styled_component(MainPage)]
@@ -41,6 +46,7 @@ pub fn main_page() -> Html {
         "#
         ).unwrap();
     let state = use_state(|| Data{language:"rust".to_string(), ..Data::default()});
+    let output_state = use_state(|| OutputState::default());
 
     let cloned_state = state.clone();
     let editor_content_callback = Callback::from(move |code: String| {
@@ -49,18 +55,25 @@ pub fn main_page() -> Html {
         )
     });
 
+    let value = output_state.clone();
     let button_callback =  {
-        let state = state.deref().clone();
+        let state_clone = state.deref().clone();
         Callback::from(move |_| {
-            let state = state.clone();
+            let output_state = output_state.clone();
+            let state_clone = state_clone.clone();
             wasm_bindgen_futures::spawn_local(async move {
+                let state_clone = state_clone.clone();
                 let response = Request::post("http://127.0.0.1:8081/api/v1")
                     .header("content-type", "application/json")
-                    .body(to_string_pretty(&state).unwrap())
+                    .body(to_string_pretty(&state_clone).unwrap())
                     .send()
                     .await
+                    .unwrap()
+                    .text()
+                    .await
                     .unwrap();
-                log!(response.text().await.unwrap());
+                let new_output_state = OutputState{output: response};
+                output_state.set(new_output_state)
             });
         })
     };
@@ -82,6 +95,9 @@ pub fn main_page() -> Html {
             <br/>
             <RunButton label="Submit" onclick={button_callback}/>
             <LanguageSelector languages={languages} default="rust" callback={select_callback}/>
+            <div>
+                <h1>{value.output.clone()}</h1>
+            </div>
         </div>
     }
 }
